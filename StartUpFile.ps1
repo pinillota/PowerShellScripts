ef37e5f44e16220785ae5fd3502093796db43413
pinillota <31477475+pinillota@users.noreply.github.com>
2017-08-30 15:07:14 +0200

    Add files via upload

---
+++ StartUpFile.ps1
@@ -0,0 +1,22 @@
+# Declare Variables
+$OutputFiles = 'C:\Users\w1000067\Desktop\Scripts\OutputFiles'
+
+# Import Modules
+. 'C:\Program Files (x86)\VMware\Infrastructure\PowerCLI\Scripts\Initialize-PowerCLIEnvironment.ps1'
+Import-Module 'C:\Program Files (x86)\WindowsPowerShell\Modules\Cisco.UCSManager\Cisco.UCSManager.psd1'
+
+# Import Functions
+. "C:\Users\w1000067\Desktop\Scripts\PowerCLI\LibreriaFuncionesPowerCLI.ps1"
+. "C:\Users\w1000067\Desktop\Scripts\PowerShellFunctions\get_ServerInfo.ps1"
+. "C:\Users\w1000067\Desktop\Scripts\PowerShellFunctions\KnowItAll.ps1"
+. "C:\Users\w1000067\Desktop\Scripts\PowerShellFunctions\get-wwn\get-wwn.ps1"
+. "C:\Users\w1000067\Desktop\Scripts\PowerShellFunctions\PowerPathFunctions\PowerPathFunctions.ps1"
+. "C:\Users\w1000067\Desktop\Scripts\PowerShellFunctions\get-ipconfig.ps1"
+. "C:\Users\w1000067\Desktop\Scripts\PowerShellFunctions\get-PhysicalLogicalRelation.ps1"
+. "C:\Users\w1000067\Desktop\Scripts\PowerShellFunctions\get-diskfree.ps1"
+. "C:\Users\w1000067\Desktop\Scripts\PowerShellFunctions\importXLS.ps1"
+. "C:\Users\w1000067\Desktop\Scripts\PowerShellFunctions\TAMIT\TamitFunctions.ps1"
+. "C:\Users\w1000067\Desktop\Scripts\PowerShellFunctions\Test-Port.ps1"
+. "C:\Users\w1000067\Desktop\Scripts\PowerShellFunctions\get-rdpsessions.ps1"
+. "C:\Users\w1000067\Desktop\Scripts\PowerShellFunctions\get-uptime.ps1"
+. "C:\Users\w1000067\Desktop\Scripts\PowerShellFunctions\PingMachine.ps1"
